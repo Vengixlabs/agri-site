e9f0ae3f405b248b641c9ab061fd571011c2e47d
zsumon <sumon143014@gmail.com>
2019-12-26 17:15:53 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(function () {
+    onscroll = () => {
 
+        const navBar = document.getElementById('m-nav');
+        const navDiv = document.getElementById('nav-div');
+        const offset = navDiv.offsetTop;
+
+        if (window.pageYOffset < offset) {
+            navBar.classList.remove('bg-dark');
+            navBar.classList.add('bg-transparent');
+        } else {
+            navBar.classList.add('bg-dark');
+            navBar.classList.remove('bg-transparent');
+        }
+    }
 });
